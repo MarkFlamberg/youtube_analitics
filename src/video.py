a6de33c70e0d731453048f6b9b70f8172003f96a
Mark Flamberg <riot.flamberg@gmail.com>
2023-06-05 23:52:03 +0300

    add исключения

--- src/video.py
+++ src/video.py
@@ -1,30 +1,38 @@
 import os
 from googleapiclient.discovery import build
-import json
+from googleapiclient.errors import HttpError
 
 
 class Video:
-
-    def __init__(self, video_id: str) -> None:
+    def __init__(self, video_id):
         self.video_id = video_id
-        self.video_info = self.get_service().videos().list(
-            part='snippet, statistics', id=self.video_id).execute()['items'][0]['snippet']
-        self.title = self.video_info['title']
-        self.url = f'https://www.youtube.com/watch?v={self.video_id}'
-        self.video_count = int(self.get_service().videos().list(
-            part='snippet, statistics', id=self.video_id).execute()['items'][0]['statistics']['viewCount'])
-        self.like_count =  int(self.get_service().videos().list(
-            part='snippet, statistics', id=self.video_id).execute()['items'][0]['statistics']['likeCount'])
+        api_key: str = os.getenv('YT_API_KEY')
+        youtube = build('youtube', 'v3', developerKey=api_key)
+        try:
+            results = youtube.videos().list(part='snippet, statistics', id=self.video_id).execute()['items']
+            if results:
+                video = results[0]['snippet']
+                self.title = video['title']
+                self.url = f'https://www.youtube.com/watch?v={self.video_id}'
+                self.view_count = int(
+                    youtube.videos().list(part='statistics', id=self.video_id).execute()['items'][0]['statistics'][
+                        'viewCount'])
+                self.like_count = int(
+                    youtube.videos().list(part='statistics', id=self.video_id).execute()['items'][0]['statistics'][
+                        'likeCount'])
+            else:
+                self.title = None
+                self.url = None
+                self.view_count = None
+                self.like_count = None
+        except HttpError:
+            self.title = None
+            self.url = None
+            self.view_count = None
+            self.like_count = None
 
     def __str__(self):
         return self.title
-
-    def get_service(self):
-        """Метод для работы с api"""
-
-        api_key: str = os.getenv('YT_API_KEY')  # API_KEY скопирован из гугла и вставлен в переменные окружения
-        service = build('youtube', 'v3', developerKey=api_key)  # создать специальный объект для работы с API
-        return service
 
 
 class PLVideo(Video):
